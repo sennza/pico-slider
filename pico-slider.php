3158f0580a058c80ea12cb98717a2b30ec7bdee3
Tarei King <tareiking@gmail.com>
2014-07-22 22:02:42 +1000

    Added initial defaults array to initiate #10

--- pico-slider.php
+++ pico-slider.php
@@ -12,6 +12,19 @@ Version: 1.0
 class Pico_Slider {
 	private static $instance;
 
+	public $defaults = array(
+		'image_width'         => 970, 		// Height in Pixels: 9999 == any height
+		'image_height'        => 9999, 		// Height in Pixels: 9999 == any height
+		'custom_meta_options' =>
+			array (
+				'box_1'           => true, 	// Boolean, true or false
+				'box_2'           => true, 	// Boolean, true or false
+				'video'           => true, 	// Boolean, true or false
+			),
+		);
+
+
+
 	static function get_instance() {
 		if ( ! self::$instance ) {
 			self::$instance = new Pico_Slider;
@@ -70,7 +83,7 @@ class Pico_Slider {
 
 		register_post_type( 'slider', $args );
 
-		add_image_size( 'slider-thumb', 970, 9999 );
+		add_image_size( 'slider-thumb', $this->defaults['image_width'], $this->defaults['image_height'] );
 	}
 
 	public function do_slider( $args ) {
@@ -97,14 +110,22 @@ class Pico_Slider {
 	}
 
 	public function add_slider_meta_boxes() {
-		add_meta_box( 'slider_video_meta_box', __( 'Video Url' ), array(
-				$this,
-				'slider_video_meta_box'
-			), 'slider', 'side', 'core' );
-		add_meta_box( 'cta_meta_box', __( 'Calls To Action' ), array(
-				$this,
-				'cta_meta_box'
-			), 'slider', 'side', 'core' );
+
+		if ( $this->defaults['custom_meta_options']['video'] ) {
+			add_meta_box( 'slider_video_meta_box', __( 'Video Url' ), array(
+					$this,
+					'slider_video_meta_box'
+				), 'slider', 'side', 'core'
+			);
+		};
+
+		if ( $this->defaults['custom_meta_options']['box_1'] || $this->defaults['custom_meta_options']['box_2'] ) {
+			add_meta_box( 'cta_meta_box', __( 'Calls To Action' ), array(
+					$this,
+					'cta_meta_box'
+				), 'slider', 'side', 'core'
+			);
+		}
 		remove_meta_box( 'postimagediv', 'slider', 'side' );
 		add_meta_box( 'postimagediv', 'Slider Image', 'post_thumbnail_meta_box', 'slider', 'side' );
 	}
@@ -134,39 +155,45 @@ class Pico_Slider {
 		<div id="slider_meta">
 			<?php
 			wp_nonce_field( plugin_basename( __FILE__ ), 'slider_nonce' );
-			$button_1_link  = get_post_meta( $post_ID, 'button_1_link', true );
-			$button_1_title = get_post_meta( $post_ID, 'button_1_title', true );
-			$button_2_link  = get_post_meta( $post_ID, 'button_2_link', true );
-			$button_2_title = get_post_meta( $post_ID, 'button_2_title', true ); ?>
 
-			<?php
-			// Button 1 Link
+			# Button 1 Link
+			if ( $this->defaults['custom_meta_options']['box_1'] ) {
+				$button_1_link  = get_post_meta( $post_ID, 'button_1_link', true );
+				$button_1_title = get_post_meta( $post_ID, 'button_1_title', true );
 			?>
 			<p>
 				<label for="button_1_link" style="width:80px; display:inline-block;"><?php _e( "Button 1 Link:" ); ?></label>
 				<input type="text" id="button_1_link" name="button_1_link" value="<?php echo wptexturize( esc_html( $button_1_link ) ); ?>" size="25" />
 			</p>
 			<?php
-			// Button 1 Title
+			# Button 1 Title
 			?>
 			<p>
 				<label for="button_1_title" style="width:80px; display:inline-block;"><?php _e( "Button 1 Title:" ); ?></label>
 				<input type="text" id="button_1_title" name="button_1_title" value="<?php echo wptexturize( esc_html( $button_1_title ) ); ?>" size="25" />
 			</p>
 			<?php
-			// Button 2 Link
+			} # End Button 1
+
+			# Button 2 Link
+			if ( $this->defaults['custom_meta_options']['box_2'] ) {
+				$button_2_link  = get_post_meta( $post_ID, 'button_2_link', true );
+				$button_2_title = get_post_meta( $post_ID, 'button_2_title', true );
 			?>
 			<p>
 				<label for="button_2_link" style="width:80px; display:inline-block;"><?php _e( "Button 2 Link:" ); ?></label>
 				<input type="text" id="button_2_link" name="button_2_link" value="<?php echo wptexturize( esc_html( $button_2_link ) ); ?>" size="25" />
 			</p>
 			<?php
-			// Button 2 Title
+			## Button 2 Title
 			?>
 			<p>
 				<label for="button_2_title" style="width:80px; display:inline-block;"><?php _e( "Button 2 Title:" ); ?></label>
 				<input type="text" id="button_2_title" name="button_2_title" value="<?php echo wptexturize( esc_html( $button_2_title ) ); ?>" size="25" />
 			</p>
+			<?php
+			} # End Buton 2
+			?>
 		</div>
 	<?php
 	}
@@ -196,11 +223,23 @@ class Pico_Slider {
 			$_POST['imagealignment'] = 'alignnone';
 		}
 		update_post_meta( $_POST['ID'], 'imagealignment', $_POST['imagealignment'] );
-		update_post_meta( $_POST['ID'], 'slider_video_url', esc_url( $_POST['slider_video_url'] ) );
-		update_post_meta( $_POST['ID'], 'button_1_link', esc_url( $_POST['button_1_link'] ) );
-		update_post_meta( $_POST['ID'], 'button_1_title', esc_html( $_POST['button_1_title'] ) );
-		update_post_meta( $_POST['ID'], 'button_2_link', esc_url( $_POST['button_2_link'] ) );
-		update_post_meta( $_POST['ID'], 'button_2_title', esc_html( $_POST['button_2_title'] ) );
+
+		# Video Box
+		if ( $this->defaults['custom_meta_options']['video'] ) {
+			update_post_meta( $_POST['ID'], 'slider_video_url', esc_url( $_POST['slider_video_url'] ) );
+		}
+
+		# CTA Button 1
+		if ( $this->defaults['custom_meta_options']['box_1'] ) {
+			update_post_meta( $_POST['ID'], 'button_1_link', esc_url( $_POST['button_1_link'] ) );
+			update_post_meta( $_POST['ID'], 'button_1_title', esc_html( $_POST['button_1_title'] ) );
+		}
+
+		#CTA Button 2
+		if ( $this->defaults['custom_meta_options']['box_1'] ) {
+			update_post_meta( $_POST['ID'], 'button_2_link', esc_url( $_POST['button_2_link'] ) );
+			update_post_meta( $_POST['ID'], 'button_2_title', esc_html( $_POST['button_2_title'] ) );
+		}
 	}
 
 	/*
